bd063bb35348a46aa221fabca594a31098980311
Sundar Rajagopal <sundarrajagopal81@gmail.com>
2023-08-13 18:16:12 -0700

    code cleanup

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Welcome back to Java8 series by Sundar...");
-        System.out.println("Thannks Sakthi...");
-    }
-}
