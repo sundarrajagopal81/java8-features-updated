6860bf6a729c2e4ebd637b7a95d192ce3bd5a058
Sundar Rajagopal <sundarrajagopal81@gmail.com>
2023-08-13 18:12:53 -0700

    java 8 exmple1 added

---
+++ src/java8/features/lambda/JavaOldVsJava8Example.java
@@ -0,0 +1,27 @@
+package src.java8.features.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaOldVsJava8Example {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Sundar", "Snow", "Mercy", "Sundar");
+        //Before Java 8
+        List<String> uniqueList = new ArrayList<>();
+        for (String name : names) {
+            if (!uniqueList.contains(name)) {
+                uniqueList.add(name);
+            }
+        }
+        System.out.println("BEFOR Java8: "+ uniqueList);
+        //After java 8
+        List<String> uniqueList1 = names.stream().distinct().collect(Collectors.toList());
+        System.out.println("In Java8: "+ uniqueList1);
+
+
+
+
+    }
+}
