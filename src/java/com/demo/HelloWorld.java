0154cd0b96d02d6d75338362d7fcffb5cbb6cc2a
Sundar Rajagopal <sundarrajagopal81@gmail.com>
2023-08-13 17:56:18 -0700

    initial checkin

--- src/java/com/demo/HelloWorld.java
+++ src/java/com/demo/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome back to Java8 series by Sundar...");
         System.out.println("Thanks Sakthi...");
+        System.out.println("lets rock...");
     }
 }
