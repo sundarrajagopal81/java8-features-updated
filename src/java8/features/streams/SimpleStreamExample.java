70839f9902ff8d7cf0db5e438187c30c88d64164
Sundar Rajagopal <sundarrajagopal81@gmail.com>
2023-08-19 12:50:40 -0700

    stream changes

--- src/java8/features/streams/SimpleStreamExample.java
+++ src/java8/features/streams/SimpleStreamExample.java
@@ -4,12 +4,12 @@ import java.util.stream.Stream;
 
 public class SimpleStreamExample {
     public static void main(String[] args) {
-        Stream<Integer> s1 = Stream.of(1,2,3,4,5,6);
-        s1.forEach(p-> System.out.println(p));
+        Stream<Integer> s1 = Stream.of(1, 2, 3, 4, 5, 6);
+        s1.forEach(p -> System.out.println(p));
 
-        Integer arr[] = new Integer[] {1,2,3,4,5};
+        Integer arr[] = new Integer[]{1, 2, 3, 4, 5};
         Stream<Integer> s2 = Stream.of(arr);
-        s2.forEach(p-> System.out.println(p));
+        s2.forEach(p -> System.out.println(p));
 
     }
 }
--- src/java8/features/streams/StreamExample1.java
+++ src/java8/features/streams/StreamExample1.java
@@ -9,13 +9,22 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StreamExample1 {
-    static Predicate<Person> height = per ->  per.getHeight() >= 140;
-    static Predicate<Person> gender = per ->  per.getGender().equals("Male");
+    static Predicate<Person> height = per -> per.getHeight() >= 140;
+    static Predicate<Person> gender = per -> per.getGender().equals("Male");
 
     public static void main(String[] args) {
         Map<String, List<String>> map = PersonRepository.getAllPersons().
                 stream().filter(height.and(gender)).collect(Collectors.toMap(
-                Person::getName, Person::getHobbies));
-        System.out.println("Person Map::"+map);
+                        Person::getName, Person::getHobbies));
+        System.out.println("Person Map::" + map);
+
+
+        List<String> personHobbies = PersonRepository.getAllPersons()//List of person
+                .stream()
+                .map(Person::getHobbies)//Stream List of <string>
+                .flatMap(List::stream) //Stream<String>
+                .distinct().collect(Collectors.toList());
+        System.out.println("####");
+        System.out.println(personHobbies);
     }
 }
