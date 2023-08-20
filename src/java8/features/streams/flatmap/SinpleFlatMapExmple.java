3011402e7734e0c8ea8a8d2546f900af00bd8e7a
Sundar Rajagopal <sundarrajagopal81@gmail.com>
2023-08-20 11:38:07 -0700

    flatmap examples :

---
+++ src/java8/features/streams/flatmap/SinpleFlatMapExmple.java
@@ -0,0 +1,23 @@
+package src.java8.features.streams.flatmap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SinpleFlatMapExmple {
+
+     public static void main(String[] args) {
+
+         List<Integer> oddNumbers = Arrays.asList(1,3,5,7,9);
+         List<Integer> evenNumbers = Arrays.asList(2,4,6,8,10);
+
+         List<List<Integer>>  listOfList = Arrays.asList(oddNumbers,evenNumbers);
+         System.out.println("BEFORE FLATTEN:"+ listOfList);
+
+         List<Integer> finalist=   listOfList.stream().flatMap(List::stream).collect(Collectors.toList());
+         System.out.println("FinalList::"+finalist);
+
+         List<Integer> finalistSorted=   listOfList.stream().flatMap(List::stream).sorted().collect(Collectors.toList());
+         System.out.println("FinalList Sored::"+finalistSorted);
+}
+}
---
+++ src/java8/features/streams/flatmap/StreamFlatMapPersonExample.java
@@ -0,0 +1,28 @@
+package src.java8.features.streams.flatmap;
+
+import src.java8.features.repo.Person;
+import src.java8.features.repo.PersonRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFlatMapPersonExample {
+
+    static List<String>  hobbies(List<Person> personList){
+
+        return personList.stream().map(Person::getHobbies).flatMap(List::stream).collect(Collectors.toList());
+    }
+
+    static List<String>  hobbiesDistinctAndSorted(List<Person> personList){
+
+        return personList.stream().map(Person::getHobbies).flatMap(List::stream).distinct().sorted().collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Persons Hobbies::"+hobbies(PersonRepository.getAllPersons()));
+
+        System.out.println("Persons Hobbies Sorted and Distinct::"+hobbiesDistinctAndSorted(PersonRepository.getAllPersons()));
+
+    }
+}
