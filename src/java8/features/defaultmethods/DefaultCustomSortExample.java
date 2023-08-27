e96edd9a5c5073ebb7532666b9ce60d035cbf0cb
Sundar Rajagopal <sundarrajagopal81@gmail.com>
2023-08-26 20:19:46 -0700

    default method changes

---
+++ src/java8/features/defaultmethods/DefaultCustomSortExample.java
@@ -0,0 +1,36 @@
+package src.java8.features.defaultmethods;
+
+import src.java8.features.repo.Person;
+import src.java8.features.repo.PersonRepository;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class DefaultCustomSortExample {
+
+    static Comparator<Person> c2 = Comparator.comparing(Person::getHeight);
+    static Comparator<Person> c1 = Comparator.comparing(Person::getName);
+    public static void main(String[] args) {
+
+        List<Person> lists= PersonRepository.getAllPersons();
+        sortByName(lists);
+        System.out.println("_______________Sort by height");
+        sortByHeight(lists);
+
+        System.out.println("$$$$$$$$$$");
+        sortNameAndHeight(lists);
+    }
+
+    static void sortByName(List<Person> personList){
+        personList.sort(c1);
+         personList.forEach(System.out::println);
+    }
+    static void sortByHeight(List<Person> personList){
+        personList.sort(c2);
+        personList.forEach(System.out::println);
+    }
+    static void sortNameAndHeight(List<Person> personList){
+        personList.sort(c1.thenComparing(c2));
+        personList.forEach(System.out::println);
+    }
+}
---
+++ src/java8/features/defaultmethods/DefaultMethodExample.java
@@ -0,0 +1,30 @@
+package src.java8.features.defaultmethods;
+
+import src.java8.features.repo.Person;
+import src.java8.features.repo.PersonRepository;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DefaultMethodExample {
+
+
+    public static void main(String[] args) {
+        List<String> nameList = PersonRepository.getAllPersons().stream().map(Person::getName).collect(Collectors.toList());
+
+        System.out.println("Before sort..");
+        System.out.println(nameList);
+        Collections.sort(nameList);
+        System.out.println("After sort...");
+        System.out.println(nameList);
+        System.out.println("after natural order");
+        nameList.sort(Comparator.naturalOrder());
+        System.out.println(nameList);
+        System.out.println("reverse order");
+        nameList.sort(Comparator.reverseOrder());
+        System.out.println(nameList);
+    }
+}
